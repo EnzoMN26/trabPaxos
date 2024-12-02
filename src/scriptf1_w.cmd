a7a51dceddbf40aec3ec120da022f086b98b164c
EnzoMN26 <enzodrao@gmail.com>
2024-12-01 21:51:14 -0300

    criacao dos scripts de automatizacao

---
+++ src/scriptf1_w.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Iniciando comandos..."
+
+start "ACCEPTOR0" cmd /k "python env.py config0 0 ACCEPTOR"
+start "ACCEPTOR1" cmd /k "python env.py config0 1 ACCEPTOR"
+start "ACCEPTOR2" cmd /k "python env.py config0 2 ACCEPTOR"
+start "LEADER0" cmd /k "python env.py config0 0 LEADER"
+start "LEADER1" cmd /k "python env.py config0 1 LEADER"
+start "REPLICA0" cmd /k "python env.py config0 0 REPLICA"
+start "REPLICA1" cmd /k "python env.py config0 1 REPLICA"
+
+echo "Todos os comandos foram concluídos!"
---
+++ src/scriptf3_w.cmd
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Iniciando comandos..."
+
+start "ACCEPTOR0" cmd /k "python env.py config0 0 ACCEPTOR"
+start "ACCEPTOR1" cmd /k "python env.py config0 1 ACCEPTOR"
+start "ACCEPTOR2" cmd /k "python env.py config0 2 ACCEPTOR"
+start "ACCEPTOR3" cmd /k "python env.py config0 3 ACCEPTOR"
+start "ACCEPTOR4" cmd /k "python env.py config0 4 ACCEPTOR"
+start "ACCEPTOR5" cmd /k "python env.py config0 5 ACCEPTOR"
+start "ACCEPTOR6" cmd /k "python env.py config0 6 ACCEPTOR"
+start "LEADER0" cmd /k "python env.py config0 0 LEADER"
+start "LEADER1" cmd /k "python env.py config0 1 LEADER"
+start "LEADER2" cmd /k "python env.py config0 2 LEADER"
+start "LEADER3" cmd /k "python env.py config0 3 LEADER"
+start "REPLICA0" cmd /k "python env.py config0 0 REPLICA"
+start "REPLICA1" cmd /k "python env.py config0 1 REPLICA"
+start "REPLICA2" cmd /k "python env.py config0 2 REPLICA"
+start "REPLICA3" cmd /k "python env.py config0 3 REPLICA"
+
+echo "Todos os comandos foram concluídos!"
